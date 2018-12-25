4d07e3768a1c9b13f704c25c50598c63dea327b7
mrpotato <mrpotato@NaoyaKitamura-no-MacBook-Air.local>
2018-12-26 01:08:34 +0900

    modify

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 # coding:utf-8
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for,jsonify
 from lib import nk_log,connect_db
 from configparser import ConfigParser
 
@@ -9,6 +9,7 @@ config.read('ini/config.ini')
 # ログ初期設定
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 logger = nk_log.nk_Log(__name__)
 logger.debug(config["name"]["value"])
 
@@ -20,16 +21,23 @@ conn.get_data("4c6031fc-0206-11e9-8e9a-0242ac110002")
 def index():
 	logger.error("Hello World!")
 	logger.debug("Hello World!")
-	return  "top"
+	rsp = conn.get_all_data()
+	return  jsonify(rsp)
 
-@app.route('/id/<ID>',methods=['GET'])
+@app.route('/data/<ID>',methods=['GET'])
 def getdata(ID):
 	#無害化処理
 	#ID=nk_filter(ID)
 	#todo file or directory
 	logger.debug("Hello World!")
-	
-	return getlist(ID)
+	rsp = conn.get_data(ID)
+	return jsonify(rsp)
+
+@app.route('/post/<DATA>',methods=['GET'])
+def postdata(DATA):
+	conn.insert_data(DATA)
+	rsp = conn.get_all_data()
+	return  jsonify(rsp)
 """
 @app.route('/post', methods=['GET', 'POST'])
 def post():
@@ -44,4 +52,4 @@ def getlist(ID):
 
 if __name__ == '__main__':
 	app.debug = True
-	app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0',port=7777)
--- lib/connect_db.py
+++ lib/connect_db.py
@@ -1,11 +1,13 @@
 # coding:utf-8
-import psycopg2 
+import psycopg2, json
 from configparser import ConfigParser
+from flask import Flask, request, redirect, url_for,jsonify
+
 config = ConfigParser()
 #config.read('~/Desktop/work/Flask/api_server/ini/config.ini')
 config.read('../ini/config.ini')
 
-connect_setting = "host=localhost port=5432 dbname=app_db user=postgres password=postgres"
+connect_setting = "host=localhost port=5432 dbname=app_db user=app_user password=app_user"
 
 class connect_DB():
     def __init__(self,name):
@@ -14,17 +16,55 @@ class connect_DB():
         with psycopg2.connect(connect_setting) as conn: 
             with conn.cursor() as cur:
                 sql =  "SELECT data FROM tb_json where id='"+id+"'"
-                cur.execute(sql)
-                for row  in cur:
-                    data = row[0]
-                print response_format("aaa");
+                try:
+                    cur.execute(sql)
+                    for row  in cur:
+                        data = row[0]
+                        rsp = response_format(200,data)
+                        return rsp
+                    return response_format(404,"No Data")
+                except Exception as e:
+                    rsp = response_format(500,"System Error")
+                    #todo
+                    # log e
+                    return rsp
+    def get_all_data(self):
+        with psycopg2.connect(connect_setting) as conn: 
+            with conn.cursor() as cur:
+                sql =  "SELECT * FROM tb_json limit 100"
+                try:
+                    cur.execute(sql)
+                    results = cur.fetchall()
+                    dict_result = []
+                    for row in results:
+                        print(row,type(row))
 
+#                    rsp = rsponse_format(200,cur.fetchall())
+                    return cur.fetchall()
+                except Exception as e:
+                    rsp = response_format(500,"System Error")
+                    #todo
+                    # log e
+                    return rsp
     def get_parent_id(self,id):
         pass
     def get_child_id(self,paret_id):
         pass
+    def insert_data(self,data):
+        with psycopg2.connect(connect_setting) as conn: 
+            with conn.cursor() as cur:
+                sql =  "INSERT INTO tb_json(id,parent_id,data) values (uuid_generate_v1(),NULL,jsonb(%s))"
+                try:
+                    cur.execute(sql, [json.dumps(data)])
+                except Exception as e:
+                    rsp = response_format(500,"System Error")
+                    print(e)
+                    return e
+                    #todo
+                    # log e
+        return "aaa"
 
 
-def response_format(str):
-    test = {"aaa":"bbb"}
-    return test
+def response_format(status,data):
+    rsp = {"status":status,"data":data}
+    return rsp
